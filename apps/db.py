2886dc71aad8c8c4d387fd4ffb73e4a2f4941a72
Mohideen Shajith <mdshaj97@gmail.com>
2026-07-19 13:34:00 +0530

    changes the infra and adds application

---
+++ apps/db.py
@@ -0,0 +1,47 @@
+from psycopg import connect
+
+
+def save_employee(data):
+
+    with connect(
+        host="localhost",
+        port=5432,
+        dbname="employee_db",
+        user="admin",
+        password="Password123"
+    ) as conn:
+
+        with conn.cursor() as cur:
+
+            cur.execute("""
+                INSERT INTO employees
+                (
+                    employee_name,
+                    dob,
+                    total_experience,
+                    relevant_experience,
+                    employer_name,
+                    notice_period,
+                    serving_notice,
+                    last_working_day,
+                    email,
+                    phone
+                )
+
+                VALUES
+                (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+            """,
+            (
+                data["employee_name"],
+                data["dob"],
+                data["total_experience"],
+                data["relevant_experience"],
+                data["employer_name"],
+                data["notice_period"],
+                data["serving_notice"],
+                data["last_working_day"],
+                data["email"],
+                data["phone"]
+            ))
+
+            conn.commit()
